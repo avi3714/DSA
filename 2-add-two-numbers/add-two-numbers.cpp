daf203db791d6fb388f965b7affc4421035d560e
avi3714 <avineetsinghmucchal@gmail.com>
2025-05-22 15:00:30 +0530

    Time: 4 ms (21.88%) | Memory: 77.2 MB (43.23%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -14,77 +14,31 @@ public:
         int carry=0;
         ListNode* temp=new ListNode(-1);
         ListNode* temp3=temp;
-        while(l1!=NULL && l2!=NULL)
+        while(l1!=NULL || l2!=NULL)
         {
-            int value;
-            if(l1->val+l2->val+carry <10)
+            int value=carry;
+            if(l1)
+            value+=l1->val;
+            if(l2)
+            value+=l2->val;
+            if(value <10)
             {
-                value=l1->val+l2->val+carry;
                 carry=0;
             }
             else
             {
-                value=l1->val+l2->val+carry-10;
+                value=value-10;
                 carry=1;
             }
             ListNode* newnode=new ListNode(value);
             temp->next=newnode;
             temp=temp->next;
+            if(l1)
             l1=l1->next;
+            if(l2)
             l2=l2->next;
         }
-        if(l1!=NULL)
-        {
-            temp->next=l1;
-            while(l1!=NULL)
-            {
-            if(l1->val+carry<10)
-            {
-                l1->val=l1->val+carry;
-                carry=0;
-            }
-            else
-            {
-                l1->val=l1->val+carry-10;
-                carry=1;
-            }
-            if(l1->next==NULL && carry==1)
-            {
-                ListNode* newi=new ListNode(1);
-                l1->next=newi;
-                l1=l1->next;
-                carry=0;
-            }
-            l1=l1->next;
-            }
-
-        }
-        if(l2!=NULL)
-        {
-            temp->next=l2;
-            while(l2!=NULL)
-            {
-            if(l2->val+carry<10)
-            {
-                l2->val=l2->val+carry;
-                carry=0;
-            }
-            else
-            {
-                l2->val=l2->val+carry-10;
-                carry=1;
-            }
-            if(l2->next==NULL && carry==1)
-            {
-                ListNode* newi=new ListNode(1);
-                l2->next=newi;
-                l2=l2->next;
-                carry=0;
-            }
-            l2=l2->next;
-            }
-
-        }
+   
         if(carry==1)
         {
              ListNode* newid=new ListNode(1);
