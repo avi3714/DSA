b7dfa2a27501806e2b54f7d150c9daf397bc8c6a
avi3714 <avineetsinghmucchal@gmail.com>
2025-06-16 19:08:15 +0530

    Time: 56 ms (18.48%) | Memory: 27.5 MB (5.51%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -11,21 +11,62 @@
  */
 class Solution {
 public:
-int heightofbinarytree(TreeNode* node,int &d)
+int heightofbinarytree(TreeNode* node,int &d,TreeNode* &highnode,unordered_map<TreeNode*,int>&mpp)
 {
        if(node==NULL)
        return 0;
-       int heightleft=heightofbinarytree(node->left,d);
-       int heightright=heightofbinarytree(node->right,d);
-       d=max(d,heightleft+heightright);
+       int heightleft=heightofbinarytree(node->left,d,highnode,mpp);
+       int heightright=heightofbinarytree(node->right,d,highnode,mpp);
+       if(heightleft+heightright>=d)
+       {
+          d=heightleft+heightright;
+          highnode=node;
+       }
+       if(heightleft>=heightright)
+       {
+        mpp[node]=0;
+       }
+       else
+       {
+        mpp[node]=1;
+       }     
        return 1+max(heightleft,heightright);
 }
     int diameterOfBinaryTree(TreeNode* root) {
         if(root ==NULL)
         return 0;          
+        unordered_map<TreeNode*,int>mpp;
         int d=0;
-        int height=heightofbinarytree(root,d);
+        TreeNode* highnode=NULL;
+        int height=heightofbinarytree(root,d,highnode,mpp);
+        
+        vector<int>v;
+        TreeNode* othernode=highnode->right;
+        while(highnode)
+        {
+            v.push_back(highnode->val);
+            if(mpp[highnode]==0)
+            highnode=highnode->left;
+            else
+            highnode=highnode->right;
+        }
+        reverse(v.begin(),v.end());
+        while(othernode)
+        {
+            v.push_back(othernode->val);
+            if(mpp[othernode]==0)
+            othernode=othernode->left;
+            else
+            othernode=othernode->right;
+        }
+        for(auto it:v)
+        {
+            cout<<it<<" ";
+        }
+
+        cout<<endl;
         return d;
+
 
         
     }
