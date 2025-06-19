1a90971335027421e51e7c10fa4d5350af441884
avi3714 <avineetsinghmucchal@gmail.com>
2025-06-19 19:37:01 +0530

    Time: 15 ms (58.08%) | Memory: 43.1 MB (47.34%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -1,35 +1,37 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        vector<int> freq(26, 0);
-        for (char task : tasks)
-            freq[task - 'A']++;
-
-        priority_queue<int> pq;
-        for (int f : freq)
-            if (f > 0)
-                pq.push(f);
-
-        queue<pair<int, int>> cooldown;
-        int time = 0;
-
-        while (!pq.empty() || !cooldown.empty()) {
-            // Move back all tasks whose cooldown has ended
-            while (!cooldown.empty() && cooldown.front().second == time) {
-                pq.push(cooldown.front().first);
-                cooldown.pop();
+        int size=tasks.size();
+        priority_queue<pair<int,int>>pq;
+        queue<pair<int,int>>q;
+        vector<int>freq(26,0);
+        for(int i=0;i<size;i++)
+        freq[tasks[i]-'A']++;
+        for(int i=0;i<26;i++)
+        {
+            if(freq[i]>0)
+        pq.push({freq[i],0});
+        }
+        int time=0;
+        while(!pq.empty() || !q.empty())
+        {
+            while(!q.empty() && q.front().second ==time)
+            {
+                pair<int,int> po=q.front();
+            pq.push(po);
+            q.pop();
             }
-
-            if (!pq.empty()) {
-                int freq = pq.top();
+            if(!pq.empty())
+            {
+                int frequency=pq.top().first;
+                int tym=pq.top().second;
                 pq.pop();
-                if (freq > 1)
-                    cooldown.push({freq - 1, time + n + 1});
+                if(frequency>1)
+                q.push({frequency-1,time+n+1});
             }
-
             time++;
         }
-
         return time;
+        
     }
 };
