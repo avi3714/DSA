5a58290bcf3a5c2f796dbad130fe9a69cc1ebe32
avi3714 <avineetsinghmucchal@gmail.com>
2025-06-20 16:16:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge k Sorted Arrays/merge-k-sorted-arrays.cpp
+++ Difficulty: Medium/Merge k Sorted Arrays/merge-k-sorted-arrays.cpp
@@ -1,25 +1,49 @@
 class Solution {
   public:
+  
+class Node{
+  public:
+  int data;
+  int row;
+  int col;
+  Node(int data,int col,int row)
+  {
+      this->data=data;
+      this->row=row;
+      this->col=col;
+  }
+};
+class compare{
+  public:
+  bool operator()(Node a,Node b){
+      return a.data > b.data;
+  }
+};
     // Function to merge k sorted arrays.
     vector<int> mergeKArrays(vector<vector<int>> arr, int K) {
-        vector<int>c;
         // code here
-        priority_queue<int,vector<int>,greater<int>>pq;
-          for(int i=0;i<K;i++)
-          {
-              for(int j=0;j<K;j++)
-              {
-                  
-               pq.push(arr[j][i]);
-              }
-              c.push_back(pq.top());
-              pq.pop();
-          }
-          while(!pq.empty())
-          {
-              c.push_back(pq.top());
-              pq.pop();
-          }
-          return c;
+        int n=K;
+        vector<int>ans;
+        priority_queue<Node,vector<Node>, compare>q;
+        for(int i=0;i<n;i++)
+        {
+            Node r= Node(arr[i][0],0,i);
+            q.push(r);
+        }
+        while(!q.empty())
+        {
+            Node front=q.top();
+            q.pop();
+            ans.push_back(front.data);
+            int row=front.row;
+            int col=front.col;
+            if(col+1 < arr[row].size())
+            {
+                q.push(Node(arr[row][col+1],col+1,row));
+            }
+        }
+        return ans;
+        
+        
     }
 };
