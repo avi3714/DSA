9c41a5add4099679e90de7c8ee28fcaeee91e947
avi3714 <avineetsinghmucchal@gmail.com>
2025-06-22 18:35:27 +0530

    Time: 29 ms (30.57%) | Memory: 46 MB (18.78%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -4,10 +4,10 @@ int f(int index,int target,vector<int>&coins,vector<vector<int>>&dp)
 {
     if(dp[index][target]!=-1)
     return dp[index][target];
-    if(index==0)
-    {   
-        return target%coins[index]==0;
-    }
+    // if(index==0)
+    // {   
+    //     return target%coins[index]==0;
+    // }
     int nottake=f(index-1,target,coins,dp);
     int take=0;
     if(target>=coins[index])
@@ -16,8 +16,25 @@ int f(int index,int target,vector<int>&coins,vector<vector<int>>&dp)
 }
     int change(int amount, vector<int>& coins) {
         int n=coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        return f(n-1,amount,coins,dp);
+        vector<vector<long >>dp(n,vector<long>(amount+1,0));
+        for(int target=0;target<=amount;target++)
+        {
+            dp[0][target]=((target%coins[0])==0);
+        }
+        for(int index=1;index<n;index++)
+        {
+            for(int target=0;target<=amount;target++)
+            {
+                int nottake=dp[index-1][target];
+                int take=0;
+                if(coins[index]<=target)
+                take=dp[index][target-coins[index]];
+                dp[index][target]=take;
+                dp[index][target]+=nottake;
+            }
+        }
+        int ans=(int)dp[n-1][amount];
+        return ans;
         
     }
 };
