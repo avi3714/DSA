ab19758d629d0b1fd2c4a5044e18913adb4f6f87
avi3714 <avineetsinghmucchal@gmail.com>
2025-06-24 18:23:29 +0530

    Time: 179 ms (57.25%) | Memory: 138.5 MB (60.27%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,56 +1,31 @@
 class Solution {
 public:
-int f(int index,int buy,int qnty,vector<int>&prices,int n,vector<vector<vector<int>>>&dp)
+int f(int index,int transaction,vector<int>& prices,vector<vector<int>>&dp)
 {
-    if(qnty>=2)
+    //B S B S
+    //if(i is even then buy)
+    if(index>=prices.size())
     return 0;
-    if(index>=n)
+    if(transaction>=4)
     return 0;
-    if(dp[index][buy][qnty]!=-1)
-    return dp[index][buy][qnty];
-     if(buy==0)
-     {
-        int a=-prices[index]+f(index+1,1,qnty,prices,n,dp);
-        int b=f(index+1,0,qnty,prices,n,dp);
-        return dp[index][buy][qnty]= max(a,b);
-     }
-     else
-     {
-        int c=prices[index]+f(index+1,0,qnty+1,prices,n,dp);
-        int d=f(index+1,1,qnty,prices,n,dp);
-        return dp[index][buy][qnty]= max(c,d);
-     }
+    if(dp[index][transaction]!=-1)
+    return dp[index][transaction];
+    if(transaction%2==0)
+    {
+        int a=-prices[index]+f(index+1,transaction+1,prices,dp);
+        int b=f(index+1,transaction,prices,dp);
+        return dp[index][transaction]=max(a,b);
+    }
+    else
+    {
+        int c=prices[index]+f(index+1,transaction+1,prices,dp);
+        int d=f(index+1,transaction,prices,dp);
+        return dp[index][transaction]=max(c,d);
+    }
 }
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-       // vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
-        vector<vector<int>>prev(2,vector<int>(3,0)),curr(2,vector<int>(3,0));
-        for(int index=n-1;index>=0;index--)
-        {
-            for(int buy=0;buy<2;buy++)
-            {
-                for(int qnty=0;qnty<2;qnty++)
-                {
-                       if(buy==0)
-                    {
-                      int a=-prices[index]+prev[1][qnty];
-                      int b=prev[0][qnty];
-                       curr[buy][qnty]= max(a,b);
-                     }
-
-                      else
-                       {
-                         int c=prices[index]+prev[0][qnty+1];
-                         int d=prev[1][qnty];
-                         curr[buy][qnty]= max(c,d);
-                       }
-          }
-            }
-        prev=curr;
-        }
-
-
-        return curr[0][0];
-        
+        vector<vector<int>>dp(n,vector<int>(4,-1));
+        return f(0,0,prices,dp);
     }
 };
