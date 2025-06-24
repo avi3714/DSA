a36a05919bbefacc3133e851ba07fa48647d53a3
avi3714 <avineetsinghmucchal@gmail.com>
2025-06-24 14:09:38 +0530

    Time: 31 ms (65.44%) | Memory: 14.8 MB (73.83%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,35 +1,5 @@
 class Solution {
 public:
- bool f(int i,int j,string s, string p,vector<vector<int>>&dp)
- {
-    if(i<0 && j<0)
-    return true;
-    if(j<0)
-    {
-        if(i<0)
-        return true;
-        else
-        return false;
-    }
-    if(i<0)
-    {
-        for(int i1=0;i1<=j;i1++)
-        {
-            if(p[i1]!='*')
-            return false;
-        }
-        return true;
-    }
-    if(dp[i][j]!=-1)
-    return dp[i][j];
-     if(s[i]==p[j] || p[j]=='?')
-     return dp[i][j]=f(i-1,j-1,s,p,dp);
-     if(p[j]=='*')
-     {
-        return dp[i][j]=f(i-1,j,s,p,dp)|f(i,j-1,s,p,dp);
-     }
-     return dp[i][j]=false;
- }
     bool isMatch(string s, string p) {
         int n1=s.length();
         int n2=p.length();
@@ -38,14 +8,11 @@ public:
         dp[0][0] = true;
       for(int j = 1; j <= n2; j++) {
          if(p[j - 1] == '*')
-        dp[0][j] = dp[0][j - 1];
+        dp[0][j] = true;
         else
         break;
          }
-
-       
-        
-        for(int i=1;i<=n1;i++)
+         for(int i=1;i<=n1;i++)
         {
             for(int j=1;j<=n2;j++)
             {
