decdecc939e7b1b1b4fef36abf46e9dc959cd77f
avi3714 <avineetsinghmucchal@gmail.com>
2025-06-27 14:08:04 +0530

    Time: 210 ms (17.60%) | Memory: 14 MB (27.62%) - LeetSync

--- 312-burst-balloons/burst-balloons.cpp
+++ 312-burst-balloons/burst-balloons.cpp
@@ -20,8 +20,22 @@ int f(int i,int j,vector<int>&nums,vector<vector<int>>&dp)
         nums.push_back(1);
         nums.insert(nums.begin(),1);
         int n=nums.size();
-        vector<vector<int>>dp(n-1,vector<int>(n-1,-1));
-        return f(1,n-2,nums,dp);
+        vector<vector<int>>dp(n-1,vector<int>(n-1,0));
+        for(int i=n-2;i>=1;i--)
+        {
+            for(int j=i;j<=n-2;j++)
+            {
+                int maxi=INT_MIN;
+                for(int ind=i;ind<=j;ind++)
+                {
+                  int ans=nums[i-1]*nums[ind]*nums[j+1];
+                  ans+=f(i,ind-1,nums,dp)+f(ind+1,j,nums,dp);
+                 maxi=max(maxi,ans);
+                }
+                dp[i][j]= maxi;
+            }
+        }
+        return dp[1][n-2];
 
     }
 };
