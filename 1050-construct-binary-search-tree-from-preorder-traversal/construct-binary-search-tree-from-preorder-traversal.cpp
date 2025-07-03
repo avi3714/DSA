fa88259c3fb0e616ac0d98b9073ae400d0bf1fb1
avi3714 <avineetsinghmucchal@gmail.com>
2025-07-03 13:33:53 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (47.40%) - LeetSync

--- 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
@@ -11,26 +11,18 @@
  */
 class Solution {
 public:
-TreeNode* constructbt(vector<int>&preorder,vector<int>&inorder,int prestart,int preend,int instart, int inend,map<int,int>&mpp)
+TreeNode* build(vector<int>&preorder,int &index,int ub)
 {
-    if(prestart>preend || instart > inend)
+    if(index>=preorder.size()|| preorder[index]>=ub )
     return NULL;
-         TreeNode* root=new TreeNode(preorder[prestart]);
-         root->left=constructbt(preorder,inorder,prestart+1,prestart+mpp[preorder[prestart]]-instart,instart,mpp[preorder[prestart]]-1,mpp);
-         root->right=constructbt(preorder,inorder,prestart+mpp[preorder[prestart]]-instart+1,preend,mpp[preorder[prestart]]+1,inend,mpp);
-         return root;
+    TreeNode* root= new TreeNode(preorder[index++]);
+    root->left=build(preorder,index,root->val);
+    root->right=build(preorder,index,ub);
+    return root;
 }
     TreeNode* bstFromPreorder(vector<int>& preorder) {
-        //so method 1 is to construct inorder and then construct a unique bst.
-        vector<int> inorder(preorder);
-        sort(inorder.begin(),inorder.end());
-        map<int,int>mpp;
-        int n=inorder.size();
-        for(int i=0;i<n;i++)
-        {
-            mpp[inorder[i]]=i;
-        }
-        TreeNode* root=constructbt(preorder,inorder,0,n-1,0,n-1,mpp);
+        int index=0;
+        TreeNode* root= build(preorder,index,INT_MAX);
         return root;
         
     }
