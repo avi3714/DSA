2488c9ba6a51337dca20642fb3a70513e1532712
avi3714 <avineetsinghmucchal@gmail.com>
2025-07-05 19:54:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
@@ -1,39 +1,37 @@
 class Solution {
   public:
-  bool dfs(int index,vector<int>&visited,vector<int>&path_visited,vector<vector<int>>&adj)
-  {
-      visited[index]=1;
-      path_visited[index]=1;
-      for(int i=0;i<adj[index].size();i++)
-      {
-          if(visited[adj[index][i]]==0)
-          {
-              if(dfs(adj[index][i],visited,path_visited,adj))
-              return true;
-          }
-          else if(path_visited[adj[index][i]]==1)
-          return true;
-      }
-      path_visited[index]=0;
-      return false;
-  }
     bool isCyclic(int V, vector<vector<int>> &edges) {
         // code here
-        vector<int>visited(V,0);
-        vector<int>path_visited(V,0);
         vector<vector<int>>adj(V);
+        vector<int>indegree(V,0);
         for(int i=0;i<edges.size();i++)
         {
             adj[edges[i][0]].push_back(edges[i][1]);
+            indegree[edges[i][1]]++;
         }
+        queue<int>q;
         for(int i=0;i<V;i++)
         {
-            if(visited[i]==0)
+            
+            if(indegree[i]==0)
+            q.push(i);
+        }
+        int count=0;
+        while(!q.empty())
+        {
+            int r=q.front();
+            q.pop();
+            count++;
+            for(int i=0;i<adj[r].size();i++)
             {
-                if(dfs(i,visited,path_visited,adj))
-                return true;
+                indegree[adj[r][i]]--;
+                if(indegree[adj[r][i]] == 0)
+                q.push(adj[r][i]);
             }
         }
+        if(count < V)
+        return true;
+        else
         return false;
     }
 };
