5c4d4929936ded3c1df3cc1c26430544ef403b12
avi3714 <avineetsinghmucchal@gmail.com>
2025-07-10 00:33:39 +0530

    Time: 10 ms (96.74%) | Memory: 50.6 MB (51.75%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.cpp
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.cpp
@@ -1,70 +1,58 @@
 struct Node{
-  Node* links[26];
-  bool flag=false;
-  
- bool containskey(char ch)
- {
-    if(links[ch-'a'] != NULL)
-    return true;
-    else
-    return false;
- }
- void put(char ch,Node* node)
- {
+Node* links[26];
+bool flag=false;
+void put(char ch,Node* node)
+{
     links[ch-'a']=node;
- }
- void setEnd()
- {
+}
+void setEnd()
+{
     flag=true;
- }
- bool isfound()
- {
+}
+bool isfound()
+{
     return flag;
- }
+}
 
 };
 class Trie {
 public:
 Node* root;
-
     Trie() {
-        root=new Node();
-        
+        root=new Node();       
     }
     
     void insert(string word) {
         Node* node=root;
         for(int i=0;i<word.length();i++)
         {
-            if(node->links[word[i] -'a'] ==NULL)
+            if(node->links[word[i] - 'a'] == NULL)
             {
                 node->put(word[i],new Node());
             }
             node=node->links[word[i] - 'a'];
         }
-        node->setEnd();
+        node->setEnd();        
     }
     
     bool search(string word) {
         Node* node=root;
         for(int i=0;i<word.length();i++)
         {
-            if(!node->containskey(word[i]))
+            if(node->links[word[i] - 'a'] == NULL)
             return false;
-            node = node->links[word[i] - 'a'];
+            node=node->links[word[i] - 'a'];
         }
         return node->isfound();
-        
     }
     
-    bool startsWith(string word) {
-         Node* node=root;
-        for(int i=0;i<word.length();i++)
+    bool startsWith(string prefix) {
+        Node* node=root;
+        for(int i=0;i<prefix.length();i++)
         {
-            if(node->links[word[i] -'a'] ==NULL)
+            if(node->links[prefix[i] - 'a'] == NULL)
             return false;
-             node = node->links[word[i] - 'a'];
-            
+            node=node->links[prefix[i] - 'a'];
         }
         return true;
         
